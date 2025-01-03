1748540a3752580476fd8f2d7f6876e90c6cbdc6
unknown <ss2001shershah@gmail.com>
2025-01-03 16:24:03 +0530

    django codes

--- app/urls.py
+++ app/urls.py
@@ -2,23 +2,46 @@ from django.urls import path
 from app import views
 from django.conf import settings
 from django.conf.urls.static  import static
+from django.contrib.auth import  views as auth_views
+from .forms import LoginForm,MyPasswordChangeForm,MyPasswordResetForm
 urlpatterns = [
     # path('', views.home),
     path('',views.ProductView.as_view(),name='home'),
+
     # path('product-detail/', views.product_detail, 
     #      name='product-detail'),
     path('product-detail/<int:pk>',views.ProductDetailView.as_view(),name='product-detail'),
+
     path('cart/', views.add_to_cart, name='add-to-cart'),
+
     path('buy/', views.buy_now, name='buy-now'),
+
     path('profile/', views.profile, name='profile'),
+
     path('address/', views.address, name='address'),
+
     path('orders/', views.orders, name='orders'),
-    path('changepassword/', views.change_password, name='changepassword'),
+    # path('changepassword/', views.change_password, name='changepassword'),
+
     path('mobile/', views.mobile, name='mobile'),
+
     path('mobile/<slug:data>',views.mobile,name='mobiledata'),
-    path('login/', views.login, name='login'),
-    path('registration/', views.customerregistration, name='customerregistration'),
+    # path('login/', views.login, name='login'),
+
+    path('accounts/login/',auth_views.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm),name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
+    path('passwordchange/',auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html',form_class=MyPasswordChangeForm,success_url='/passwordchangedone/'),name="passwordchange"),
+
+    path('passwordchangedone/',auth_views.PasswordChangeView.as_view(template_name='app/passwordchangedone.html'),name='passwordchangedone'),
+
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=MyPasswordResetForm)),
+    # path('registration/', views.customerregistration, name='customerregistration'),
+    path('registration/',views.CustomerRegistrationView.as_view(),name='customerregistration'),
+
     path('checkout/', views.checkout, name='checkout'),
+
 ] +  static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Customer,Product,Cart,OrderPlaced
+from .forms import CustomerRegistrationForm
+from django.contrib import messages
 # def home(request):
 #  return render(request, 'app/home.html')
 
@@ -53,11 +55,21 @@ def mobile(request,data=None):
     mobiles = Product.objects.filter(category='M').filter(discounted_price__gt=10000)
  return render(request,'app/mobile.html',{'mobiles':mobiles})
 
-def login(request):
- return render(request, 'app/login.html')
+# def login(request):
+#  return render(request, 'app/login.html')
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+# def customerregistration(request):
+#  return render(request, 'app/customerregistration.html')
 
+class CustomerRegistrationView(View):
+ def get(self,request):
+  form =  CustomerRegistrationForm()
+  return render(request,'app/customerregistration.html',{'form':form})
+ def post(self,request):
+  form = CustomerRegistrationForm(request.POST)
+  if form.is_valid():
+   messages.success(request,'Congratulation!! Registered Successfully')
+   form.save()
+  return render(request,'app/customerregistration.html',{'form':form})
 def checkout(request):
  return render(request, 'app/checkout.html')
