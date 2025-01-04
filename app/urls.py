77c1a8fb14728211c9503cb368d9ebd2152f9463
unknown <ss2001shershah@gmail.com>
2025-01-04 06:34:31 +0530

    create password reset section

--- app/urls.py
+++ app/urls.py
@@ -3,13 +3,11 @@ from app import views
 from django.conf import settings
 from django.conf.urls.static  import static
 from django.contrib.auth import  views as auth_views
-from .forms import LoginForm,MyPasswordChangeForm,MyPasswordResetForm
+from .forms import LoginForm,MyPasswordChangeForm,MyPasswordResetForm,MySetPasswordForm
 urlpatterns = [
     # path('', views.home),
     path('',views.ProductView.as_view(),name='home'),
 
-    # path('product-detail/', views.product_detail, 
-    #      name='product-detail'),
     path('product-detail/<int:pk>',views.ProductDetailView.as_view(),name='product-detail'),
 
     path('cart/', views.add_to_cart, name='add-to-cart'),
@@ -21,13 +19,11 @@ urlpatterns = [
     path('address/', views.address, name='address'),
 
     path('orders/', views.orders, name='orders'),
-    # path('changepassword/', views.change_password, name='changepassword'),
-
+    
     path('mobile/', views.mobile, name='mobile'),
 
     path('mobile/<slug:data>',views.mobile,name='mobiledata'),
-    # path('login/', views.login, name='login'),
-
+   
     path('accounts/login/',auth_views.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm),name='login'),
 
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
@@ -36,8 +32,17 @@ urlpatterns = [
 
     path('passwordchangedone/',auth_views.PasswordChangeView.as_view(template_name='app/passwordchangedone.html'),name='passwordchangedone'),
 
-    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=MyPasswordResetForm)),
-    # path('registration/', views.customerregistration, name='customerregistration'),
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=MyPasswordResetForm),name='password_reset'),
+   
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'),name="password_reset_done"),
+
+   path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='app/password_reset_confirm.html',
+        form_class=MySetPasswordForm
+    ), name='password_reset_confirm'),
+
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_confirm.html'),name="password_reset_confirm"),
+
     path('registration/',views.CustomerRegistrationView.as_view(),name='customerregistration'),
 
     path('checkout/', views.checkout, name='checkout'),
